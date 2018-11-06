1e23cb42f404eb3918dda884c8cc0155cbfad734
AhmedMohamedTalaat <ahmedtal3t.at@gmail.com>
2018-11-06 10:13:06 +0200

    Add execte query function

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
+##Project Description:
+This project is used to make analysis on large data and try to get info by using queries and this is the first project in Full-stack Nanodegree.
+###
+##Requirment:
+- Vagrant.
+- virtualbox.
+- Python 3.6
+- PostgreSQL.
+- psycopg2
+###
+##
+##Database that used:
+- we used news database that provide there tables that we used ('author,articles,log).
 ##Function that return data from "news"
 There are three functions that read from news database **'author,article,log'** :
 - **view_popular_three_article()**: this function for select three top article from articles table.
 - **view_most_pop_article_author()**:this function for select three pop author from authors table.
 - **view_log_state()**: this function for view more than 1% of requests lead to errors.
 ##Create View for Error State:
-```create view all_status as select count(log.status)as total_state, date(log."time") as time_ from log  group by time_ ;```
+- ```sql 
+       create view all_status as
+       select count(log.status)as total_state, date(log."time") as time_ 
+       from log  group by time_ ;
+     ```
 ######
-```create view error_status as select count(log.status)as total_error_state, date(log."time") as time_ from log where log.status like '404%'  group by time_;```
+- ```sql 
+        create view error_status as 
+        select count(log.status)as total_error_state, date(log."time") as time_ 
+        from log where log.status like '404%'  group by time_;
+     ```
--- logProject.py
+++ logProject.py
@@ -1,52 +1,64 @@
+#!/usr/bin/python3.6
 import psycopg2
 
 
+def execute_query(query):
+    try:
+        db = psycopg2.connect(database='news')
+        cur = db.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+        db.close()
+        return result
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
 def view_popular_three_article():
-    db = psycopg2.connect(database='news')
-    cur = db.cursor()
-    query = "select title,count(title) as total from articles join log on " \
-            " log.path=concat('/article/',articles.slug)group by title order by total desc limit 3; "
-    cur.execute(query)
-    data = cur.fetchall()
-    db.close()
-    print("View popular three article :")
+    """This function for view popular three articles from articles table"""
+    query = """
+            select title,count(title) as total from articles join log on 
+            log.path=concat('/article/',articles.slug)group by title order by total desc limit 3;
+            """
+    data = execute_query(query)
+    print("* View popular three article :")
     for row in data:
-        print(str(row[0]) + "-" + str(row[1]) + " " + "views")
+        print('"{}" - {} views'.format(str(row[0]), str(row[1])))
 
 
 def view_most_pop_article_author():
-    db = psycopg2.connect(database='news')
-    cur = db.cursor()
-    query = "select name,count(articles.title) as total from authors join articles on " \
-            " authors.id=articles.author join log on log.path=concat('/article/',articles.slug) " \
-            " group by name order by total desc limit 3;"
-    cur.execute(query)
-    data = cur.fetchall()
-    db.close()
-    print("View most pop article author :")
+    """This function for view most populart articles for authors from articles and author tables"""
+    query = """
+            select name,count(articles.title) as total from authors join articles on 
+            authors.id=articles.author join log on log.path=concat('/article/',articles.slug)
+            group by name order by total desc limit 3;
+            """
+    data = execute_query(query)
+    print("* View most pop article author :")
     for row in data:
-        print(str(row[0]) + str(row[1]) + " " + "views")
+        print('"{}" - {} views'.format(str(row[0]), str(row[1])))
 
 
 def view_log_state():
-    db = psycopg2.connect(database='news')
-    cur = db.cursor()
-    cur.execute("select all_status.time_,round((100.0*error_status.total_error_state/all_status.total_state),2) as per "
-                " from error_status,all_status where "
-                " error_status.time_=all_status.time_ "
-                " and round((100.0*error_status.total_error_state/all_status.total_state),2) > 0.01 "
-                " order by all_status.time_  desc;")
-    data = cur.fetchall()
-    db.close()
-    print("View log state :")
+    """This function for printing error persentage that exceed 1%"""
+    query = """
+                select all_status.time_,round((100.0*error_status.total_error_state/all_status.total_state),2) as per 
+                from error_status,all_status where 
+                error_status.time_=all_status.time_ 
+                and round((100.0*error_status.total_error_state/all_status.total_state),2) > 1
+                order by all_status.time_  desc;
+             """
+    data = execute_query(query)
+    print("* View log state :")
     for row in data:
         print(str(row[0]) + " - " + str(row[1]) + "%")
 
 
-view_popular_three_article()
-print('\n')
-view_most_pop_article_author()
-print('\n')
-view_log_state()
-print('\n')
-print("End of App.")
+if __name__ == '__main__':
+    view_popular_three_article()
+    print('\n')
+    view_most_pop_article_author()
+    print('\n')
+    view_log_state()
+    print('\n')
+    print("End of App.")
